ae9ab08f4f593d4752b74a48c5d28d6ad7da327e
Jiruj <55579110+Jiruj@users.noreply.github.com>
2020-07-15 11:20:46 +0200

    regex2

--- phoneNumberRegEx/main.js
+++ phoneNumberRegEx/main.js
@@ -1,17 +1,22 @@
+var phoneNumberString = "49212312222221";
+
 function formatPhoneNumber(phoneNumberString) {
-	var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-	var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
-	if (match) {
-	  var intlCode = (match[1] ? '49' : '')
-	  return [intlCode, match[2], match[3], match[4]].join('')
-	}
-	return null
+
+  alert(phoneNumberString);
+  var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+  var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+
+  if (phoneNumberString.match(/\D/g)) {
+    return false;
   }
-  
-phonenumberlength:
+  if (match) {
+    var intlCode = match[1] ? "49" : "";
+    return [intlCode, match[2], match[3], match[4]].join("");
+  }
+  if (phone.length >= 17) {
+    return false;
+  }
+  return null;
+}
 
-	if (phone.length >= 17 || cleaned.match(/\D/g)) {
-
-        return false;
-	}
-	
+formatPhoneNumber();
