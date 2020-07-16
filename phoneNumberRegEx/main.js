780c57302fd373cdfef9f0458f80b5b650188329
Jiruj <55579110+Jiruj@users.noreply.github.com>
2020-07-16 23:51:47 +0200

    final regEx phone format
    Bonus.

--- phoneNumberRegEx/main.js
+++ phoneNumberRegEx/main.js
@@ -1,44 +1,29 @@
-var phoneNumberString = "49212312222221";
-
-function formatPhoneNumber(phoneNumberString) {
-
-  alert(phoneNumberString);
-  var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
-  var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-
-  if (phoneNumberString.match(/\D/g)) {
-    return false;
-  }
-  if (match) {
-    var intlCode = match[1] ? "49" : "";
-    return [intlCode, match[2], match[3], match[4]].join("");
-  }
-  if (phone.length >= 17) {
-    return false;
-  }
-  return null;
-}
-
-formatPhoneNumber();
-
-Version 2:
-
 function normalize(phone) {
-  //normalize string and remove all unnecessary characters
-  phone = phone.replace(/[^\d]/g, "");
 
-  //check if number length equals to 10
-  if (phone.length >= 17) {
-    //reformat and return phone number
-    return false
-  } else {
-    return phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1$2$3");
+if (phone.match(/^\+.*/)){
+    // ersetze plus an erster stelle durch leeren string
+    phone = phone.substring(1);
+    }
 
+// check auf non digits wenn welche drin return false
+if( phone.match(/[^\s\d]/)){
+    return false;
+    }
+
+if( phone.match(/^00.*/) ){    
+  phone = phone.substr(2); // lösche erste zwei zeichen
+ }
+else if( phone.match(/^(0{1}[^0])(.*)/) ) {
+  phone = phone.substr(1); // lösche erstes zeichen
+  phone = "49" + phone; // prepend 49
+ }
+
+if (phone.length >= 17) {
+   return false;
+} else {
+  return phone;  
   }
-
-  return null;
 }
-
-var phone = '(123)456781195345';
+var phone = '491523456781235';
 phone = normalize(phone);
 alert(phone);
